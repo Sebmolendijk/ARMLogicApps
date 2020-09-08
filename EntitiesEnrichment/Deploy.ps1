36e2791f25174e9e13de35ede8539107d8756fda
Sebmolendijk <sebmolendijk@gmail.com>
2020-09-08 18:02:30 +0200

    V1.0

--- EntitiesEnrichment/Deploy.ps1
+++ EntitiesEnrichment/Deploy.ps1
@@ -1,6 +1,6 @@
 $today=Get-Date -Format "MM-dd-yyyy"
 $suffix = Get-Random -Maximum 100
-$deploymentName="InfrequentCountryTriageLogicApp_" + $today + "_$suffix"
+$deploymentName="EntitiesEnrichment_" + $today + "_$suffix"
 
 Import-Module Az.Resources
-New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\MdatpComputerEnrichment.template.json -TemplateParameterFile .\aadSP.parameters.json -Name $deploymentName -Verbose
+New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\UserEnrichment.template.json -TemplateParameterFile "C:\GitHub\parameters\aadSP.parameters.json" -Name $deploymentName -Verbose
