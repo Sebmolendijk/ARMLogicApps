289f1642cc71ed136a5a64cb68fd1f45778d337a
Sebmolendijk <sebmolendijk@gmail.com>
2020-09-09 13:37:35 +0200

    Readme added

--- McasAlertsInvestigations/InfrequentCountry/Deploy.ps1
+++ McasAlertsInvestigations/InfrequentCountry/Deploy.ps1
@@ -3,4 +3,5 @@ $suffix = Get-Random -Maximum 100
 $deploymentName="InfrequentCountryTriageLogicApp_" + $today + "_$suffix"
 
 Import-Module Az.Resources
-New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\InfrequentCountryTriage.template.json -TemplateParameterFile ".\mcasToken.parameters.json" -Name $deploymentName -Verbose
+New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\azuredeploy.json -TemplateParameterFile .\parameters.json -Name $deploymentName -Verbose
+
