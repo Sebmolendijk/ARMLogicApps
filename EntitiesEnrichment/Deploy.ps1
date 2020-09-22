acec835de901516b412ba7caf1251a543b8a5310
Sebmolendijk <sebmolendijk@gmail.com>
2020-09-22 15:05:55 +0200

    UserEnrichment final

--- EntitiesEnrichment/Deploy.ps1
+++ EntitiesEnrichment/Deploy.ps1
@@ -3,6 +3,6 @@ $suffix = Get-Random -Maximum 100
 $deploymentName="EntitiesEnrichment_" + $today + "_$suffix"
 
 Import-Module Az.Resources
-#New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\UserEnrichment.template.json -TemplateParameterFile "C:\GitHub\parameters\aadSP.parameters.json" -Name $deploymentName -Verbose
+New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\UserEnrichment.template.json -TemplateParameterFile C:\GitHub\parameters\mcasToken.parameters.json -Name $deploymentName -Verbose
 
-New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\Get-AlertEntitiesEnrichment.json -Name $deploymentName -Verbose
+#New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\Get-AlertEntitiesEnrichment.json -Name $deploymentName -Verbose
