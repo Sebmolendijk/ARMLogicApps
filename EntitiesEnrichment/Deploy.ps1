bcf49ece5d0e51032553a5ef5889af5cdaf83785
Sebmolendijk <sebmolendijk@gmail.com>
2020-09-22 15:18:52 +0200

    Update deployment

--- EntitiesEnrichment/Deploy.ps1
+++ EntitiesEnrichment/Deploy.ps1
@@ -1,8 +1,8 @@
 $today=Get-Date -Format "MM-dd-yyyy"
 $suffix = Get-Random -Maximum 100
-$deploymentName="EntitiesEnrichment_" + $today + "_$suffix"
+$deploymentName="_" + $today + "_$suffix"
 
 Import-Module Az.Resources
-New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\UserEnrichment.template.json -TemplateParameterFile C:\GitHub\parameters\mcasToken.parameters.json -Name $deploymentName -Verbose
+New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\UserEnrichment.template.json -TemplateParameterFile C:\GitHub\parameters\mcasToken.parameters.json -Name "UserEnrichment_$deploymentName" -Verbose
 
-#New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\Get-AlertEntitiesEnrichment.json -Name $deploymentName -Verbose
+New-AzResourceGroupDeployment -ResourceGroupName Dev -TemplateFile .\Get-AlertEntitiesEnrichment.json -Name "Get-AlertEntitiesEnrichment_$deploymentName" -Verbose
